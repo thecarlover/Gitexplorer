cc8887b59eae342fcdcc880272c37e41fbb8a909
Ojasvi Mittal <79406282+thecarlover@users.noreply.github.com>
2024-05-25 13:53:18 +0530

    Update environment.secret.ts

--- src/environments/environment.secret.ts
+++ src/environments/environment.secret.ts
@@ -2,6 +2,6 @@
 
 export const environment = {
     production: false,
-    githubAccessToken: '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'
+    githubAccessToken: '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'
   };
   
