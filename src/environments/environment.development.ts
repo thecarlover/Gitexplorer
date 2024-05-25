19fce163e9f4257955cc67e31a72bfb81c2be5b9
Ojasvi Mittal <79406282+thecarlover@users.noreply.github.com>
2024-05-25 14:53:05 +0530

    Delete src/environments directory

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    githubAccessToken: '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'
-  };
-  
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    githubAccessToken: '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'
-  };
-  
